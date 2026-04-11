f4b293b4a095fdb92e76e72eedd0c57eea640730
aurelioo29 <aureliolucio5@gmail.com>
2026-04-11 09:10:21 +0700

    feat: add all routes into index

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const authRoutes = require("./auth.routes");
+const emergencyReportRoutes = require("./emergencyReport.routes");
+const dispatchRoutes = require("./dispatch.routes");
+const officerLocationRoutes = require("./officerLocation.routes");
+const hospitalRoutes = require("./hospital.routes");
+const officerRoutes = require("./officer.routes");
+const ambulanceRoutes = require("./ambulance.routes");
+const userProfileRoutes = require("./userProfile.routes");
+
+// route modules
+router.use("/auth", authRoutes);
+router.use("/emergency-reports", emergencyReportRoutes);
+router.use("/dispatches", dispatchRoutes);
+router.use("/officer-locations", officerLocationRoutes);
+router.use("/hospitals", hospitalRoutes);
+router.use("/officers", officerRoutes);
+router.use("/ambulances", ambulanceRoutes);
+router.use("/users", userProfileRoutes);
+
+module.exports = router;
